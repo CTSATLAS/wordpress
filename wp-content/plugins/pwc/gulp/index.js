0c6bc7df92301fecea14596c70b086628eac9fdb
Shawn Sandy <shawnsandy@atlas.shawn>
2014-12-17 10:32:35 -0500

    installed several plugins
    deleted pwc theme form plugins directory

--- wp-content/plugins/pwc/gulp/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by shawnsandy on 8/13/14.
- */
-
-var requireDir = require('require-dir');
-
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./tasks', { recurse: true });
