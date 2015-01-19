2d04e805a8f08862fba34ddb181d41d24530f448
Shawn Sandy <shawnsandy@atlas.shawn>
2015-01-19 10:14:14 -0500

    replaced default login logo

--- wp-content/themes/panwar-theme/functions.php
+++ wp-content/themes/panwar-theme/functions.php
@@ -6,3 +6,13 @@
  * and open the template in the editor.
  */
 
+//* Replace WordPress login logo with your own
+add_action('login_head', 'pwc_custom_login_logo');
+function pwc_custom_login_logo() {
+   
+     echo '<style type="text/css">
+        h1 a { background-image:url('. get_bloginfo( 'stylesheet_directory' ) .'/images/pwc-web-logo.png) !important; }
+    </style>';
+    
+    
+}
