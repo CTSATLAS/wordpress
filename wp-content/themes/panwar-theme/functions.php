46a50f4208faa184eff004b8c16b6293c39be7c1
Shawn Sandy <shawnsandy@atlas.shawn>
2015-03-09 12:03:44 -0400

    updated slideshows

--- wp-content/themes/panwar-theme/functions.php
+++ wp-content/themes/panwar-theme/functions.php
@@ -94,3 +94,14 @@ register_post_type('pwcevents', array(
   'parent' => 'Parent pwcevent',
 )
 ) ); }
+
+add_filter('widget_text', 'do_shortcode');
+
+$ml_widget = MpLoader\Utilities\Widget::factory();
+$ml_widget->add_widget('panwar_slideshow', 'Slideshow', 'Slideshow');
+add_filter('timber_context', 'add_to_timber');
+function add_to_timber($data) {
+    $sidebars['slideshows'] = Timber::get_widgets('panwar_slideshow');
+    $data['theme_sidebars'] = $sidebars;
+    return $data;
+}
